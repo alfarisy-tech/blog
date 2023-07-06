165baa2e4493102adaea2e400b9781e968cccece
alfarisy-tech <ariafsundoro@gmail.com>
2023-07-06 08:36:06 +0700

    part 1 : create API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(RolesTableSeeder::class);
+        // $this->call(RolesTableSeeder::class);
         // $this->call(PermissionsTableSeeder::class);
-        // $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,29 @@ Route::group(['middleware' => 'auth:api'], function () {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//group route with prefix "admin"
+Route::prefix('admin')->group(function () {
+
+    //group route with middleware "auth:api"
+    Route::group(['middleware' => 'auth:api'], function () {
+
+        //dashboard
+        Route::get('/dashboard', App\Http\Controllers\Api\Admin\DashboardController::class);
+
+        //permissions
+        Route::get('/permissions', [\App\Http\Controllers\Api\Admin\PermissionController::class, 'index'])
+            ->middleware('permission:permissions.index');
+
+        //permissions all
+        Route::get('/permissions/all', [\App\Http\Controllers\Api\Admin\PermissionController::class, 'all'])
+            ->middleware('permission:permissions.index');
+
+        //roles all
+        Route::get('/roles/all', [\App\Http\Controllers\Api\Admin\RoleController::class, 'all'])
+            ->middleware('permission:roles.index');
+
+        //roles
+        Route::apiResource('/roles', App\Http\Controllers\Api\Admin\RoleController::class)
+            ->middleware('permission:roles.index|roles.store|roles.update|roles.delete');
+    });
+});
