66f4f96f253304b6951ce4853b9dc3986cf8874b
alfarisy-tech <ariafsundoro@gmail.com>
2023-07-07 14:31:32 +0700

    api admin clear

--- routes/api.php
+++ routes/api.php
@@ -49,5 +49,25 @@ Route::prefix('admin')->group(function () {
         //roles
         Route::apiResource('/roles', App\Http\Controllers\Api\Admin\RoleController::class)
             ->middleware('permission:roles.index|roles.store|roles.update|roles.delete');
+
+        //users
+        Route::apiResource('/users', App\Http\Controllers\Api\Admin\UserController::class)
+            ->middleware('permission:users.index|users.store|users.update|users.delete');
+
+        //categories all
+        Route::get('/categories/all', [\App\Http\Controllers\Api\Admin\CategoryController::class, 'all'])
+            ->middleware('permission:categories.index');
+
+        //Categories
+        Route::apiResource('/categories', App\Http\Controllers\Api\Admin\CategoryController::class)
+            ->middleware('permission:categories.index|categories.store|categories.update|categories.delete');
+
+        //Posts
+        Route::apiResource('/posts', App\Http\Controllers\Api\Admin\PostController::class)
+            ->middleware('permission:posts.index|posts.store|posts.update|posts.delete');
+
+        //Sliders
+        Route::apiResource('/sliders', App\Http\Controllers\Api\Admin\SliderController::class, ['except' => ['create', 'show', 'update']])
+            ->middleware('permission:sliders.index|sliders.store|sliders.delete');
     });
 });
